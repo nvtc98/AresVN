da1a99ddc2eb454ca51eb155e46f35de14904d8f
Thanh Chương <nvtc.98@gmail.com>
2022-01-12 17:46:12 +0700

    init animation & chart libraries for Team

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,31 +1,17 @@
-import RadarChart from "react-svg-radar-chart";
-// import "react-svg-radar-chart/build/css/index.css";
-
-const data = [
-  {
-    "data": {
-     "battery": 0.7,
-     "design": 0.8,
-     "useful": 0.9,
-     "speed": 0.67,
-     "weight": 0.8
-    },
-    "meta": {
-     "color": "#edc951"
-    }
-   }
-];
-
-const captions = {
-  // columns
-  battery: "Aiming",
-  design: "Movement",
-  useful: "Stability",
-  speed: "Timing",
-  weight: "Weight",
-};
+import { useRef, useState } from "react";
+import ApexCharts from "apexcharts";
+import { motion } from "framer-motion";
+//https://www.npmjs.com/package/framer-motion
+//react-animations
+//https://github.com/brunnolou/react-morph    https://codesandbox.io/s/jpnq33q47w?from-embed=&file=/src/Components/PlayerMini.js:96-102
 
 export const Team = (props) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const contentRef = useRef({
+    currentItem: null,
+    nextItem: null,
+  });
+
   return (
     <div id="team" className="text-center">
       <div className="container">
@@ -38,17 +24,39 @@ export const Team = (props) => {
             mainly in 2021).
           </p>
         </div>
-        <div id="row">
+        {props.data ? (
+          <div className="col-md-12">
+            <motion.div
+              key={selectedIndex}
+              // animate={{ x: selectedIndex * 100 }}
+              className="col-md-1 col-sm-1 team"
+              onClick={() => setSelectedIndex(selectedIndex + 1)}
+            >
+              <img src={props.data[selectedIndex].img} className="team-img" />
+            </motion.div>
+            {/* <motion.div
+              animate={{ x: selectedIndex * 100 }}
+              onClick={() => setSelectedIndex(selectedIndex + 1)}
+              className="col-md-1 col-sm-1 team"
+            >
+              <img
+                src={contentRef.current.nextItem.img}
+                className="team-img"
+              />
+            </motion.div> */}
+          </div>
+        ) : (
+          "loading"
+        )}
+        <div id="row" className="col-md-12 section-title">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                <div key={`${d.name}-${i}`} className="col-md-1 col-sm-1 team">
                   <div className="thumbnail">
                     {" "}
-                    <img src={d.img} alt="..." className="team-img" />
+                    <img src={d.img} alt={d.name} className="team-img" />
                     <div className="caption">
-                      <h4>{d.name}</h4>
-                      <p>{d.job}</p>
-                      <RadarChart captions={captions} data={data} size={200} />
+                      <div>{d.name}</div>
                     </div>
                   </div>
                 </div>
