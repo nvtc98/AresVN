77798ac0ce05d9bc337439595a23e6b4ecf83766
Thanh Chương <nvtc.98@gmail.com>
2022-01-14 17:46:50 +0700

    init animation for Team

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -9,6 +9,19 @@ export const Team = (props) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [isAnimating, setAnimating] = useState(false);
 
+  const contentRef = useRef({
+    lastIndex: 0,
+  });
+
+  const onChangePlayer = (index) => {
+    contentRef.current.lastIndex = selectedIndex;
+    setSelectedIndex(index);
+    setAnimating(true);
+    setTimeout(() => {
+      setAnimating(false);
+    }, 500);
+  };
+
   return (
     <div id="team" className="text-center">
       <div className="container">
@@ -23,24 +36,42 @@ export const Team = (props) => {
         </div>
         {props.data ? (
           <div className="col-md-12">
-            <motion.div
-              key={selectedIndex}
-              animate={{ x: selectedIndex * 100 }}
-              className="col-md-1 col-sm-1 team"
-              onClick={() => setSelectedIndex(selectedIndex + 1)}
+            <div
+              className="card col-md-10 col-md-offset-1"
+              style={{ padding: 40 }}
             >
-              <img src={props.data[selectedIndex].img} className="team-img" />
-            </motion.div>
-            {/* <motion.div
-              animate={{ x: selectedIndex * 100 }}
-              onClick={() => setSelectedIndex(selectedIndex + 1)}
-              className="col-md-1 col-sm-1 team"
-            >
-              <img
-                src={contentRef.current.nextItem.img}
-                className="team-img"
-              />
-            </motion.div> */}
+              <div className="col-md-4 col-sm-4 team">
+                <motion.img
+                  animate={{ x: selectedIndex * 100 }}
+                  src={props.data[selectedIndex].img}
+                  className="team-img text-left"
+                  style={{ borderRadius: 10 }}
+                  onClick={() => setSelectedIndex(selectedIndex + 1)}
+                />
+                <div className="animation-block">
+                  <motion.h3
+                    // animate={{ x: isAnimating ? -200 : 0 }}
+                    style={{ opacity: 1 }}
+                  >
+                    {props.data[contentRef.current.lastIndex].name}
+                  </motion.h3>
+                  <motion.h3
+                    {...(isAnimating
+                      ? {
+                          animate: {
+                            x: isAnimating ? 0 : 200,
+                          },
+                        }
+                      : {})}
+                    style={{ x: 200, opacity: isAnimating ? 1 : 0 }}
+                  >
+                    {props.data[selectedIndex].name}
+                  </motion.h3>
+                </div>
+                <motion.p>{props.data[selectedIndex].role}</motion.p>
+              </div>
+              <div className="col-md-8 col-sm-8">hehe</div>
+            </div>
           </div>
         ) : (
           "loading"
@@ -48,7 +79,11 @@ export const Team = (props) => {
         <div id="row" className="col-md-12 section-title">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-1 col-sm-1 team">
+                <div
+                  key={`${d.name}-${i}`}
+                  className="col-md-1 col-sm-1 team"
+                  onClick={() => onChangePlayer(i)}
+                >
                   <div className="thumbnail">
                     {" "}
                     <img src={d.img} alt={d.name} className="team-img" />
