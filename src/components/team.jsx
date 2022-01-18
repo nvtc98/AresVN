384b39342f7c94379a30b12c1248bc713558b236
Thanh Chương <nvtc.98@gmail.com>
2022-01-18 14:37:31 +0700

    update player data for Team

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -40,7 +40,7 @@ export const Team = (props) => {
       var chartLabel = document.createTextNode(playerData.label[i].name);
       element.appendChild(chartLabel);
     }
-  }, [props]);
+  }, [playerData]);
 
   useEffect(() => {
     for (let i = 0; i < 3; ++i) {
@@ -62,83 +62,77 @@ export const Team = (props) => {
             mainly in 2021).
           </p>
         </div>
-        {props.data ? (
-          <div className="col-md-12">
-            <div
-              className="card col-md-10 col-md-offset-1"
-              style={{ padding: 40 }}
-            >
-              <div className="col-md-4 col-sm-12 team">
-                <AnimatePresence exitBeforeEnter>
-                  <motion.img
-                    key={selectedIndex}
-                    src={props.data[selectedIndex].img}
-                    className="team-img text-left"
-                    style={{ borderRadius: 10 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    initial={{ opacity: 0, y: 20 }}
-                    exit={{ opacity: 0, y: -20 }}
-                    transition={{ duration: 0.1 }}
-                  />
-                </AnimatePresence>
-                <div className="animation-block">
-                  <motion.h3 style={{ opacity: isAnimating ? 0 : 1 }}>
-                    {props.data[contentRef.current.lastIndex].name}
-                  </motion.h3>
-                  <motion.h3
-                    animate={{ x: isAnimating ? 0 : 200 }}
-                    style={{ x: 200, opacity: isAnimating ? 1 : 0 }}
-                  >
-                    {props.data[selectedIndex].name}
-                  </motion.h3>
-                </div>
-                <div className="animation-block">
-                  <motion.p style={{ opacity: isAnimating ? 0 : 1 }}>
-                    {props.data[contentRef.current.lastIndex].role}
-                  </motion.p>
-                  <motion.p
-                    animate={{ x: isAnimating ? 0 : -200 }}
-                    style={{ x: -200, opacity: isAnimating ? 1 : 0 }}
-                  >
-                    {props.data[selectedIndex].role}
-                  </motion.p>
-                </div>
+        <div className="col-md-12">
+          <div
+            className="card col-md-10 col-md-offset-1"
+            style={{ padding: 40 }}
+          >
+            <div className="col-md-4 col-sm-12 team">
+              <AnimatePresence exitBeforeEnter>
+                <motion.img
+                  key={selectedIndex}
+                  src={playerData.data[selectedIndex].img}
+                  className="team-img text-left"
+                  style={{ borderRadius: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  initial={{ opacity: 0, y: 20 }}
+                  exit={{ opacity: 0, y: -20 }}
+                  transition={{ duration: 0.1 }}
+                />
+              </AnimatePresence>
+              <div className="animation-block">
+                <motion.h3 style={{ opacity: isAnimating ? 0 : 1 }}>
+                  {playerData.data[contentRef.current.lastIndex].name}
+                </motion.h3>
+                <motion.h3
+                  animate={{ x: isAnimating ? 0 : 200 }}
+                  style={{ x: 200, opacity: isAnimating ? 1 : 0 }}
+                >
+                  {playerData.data[selectedIndex].name}
+                </motion.h3>
               </div>
-              <div className="col-md-8 col-sm-12">
-                {playerData.label.map((chart, index) => (
-                  <>
-                    <div
-                      key={index}
-                      className="col-md-4 col-sm-4"
-                      id={"player-chart-" + index}
-                    />
-                    {/* <div>{chart.name}</div> */}
-                  </>
-                ))}
+              <div className="animation-block">
+                <motion.p style={{ opacity: isAnimating ? 0 : 1 }}>
+                  {playerData.data[contentRef.current.lastIndex].role}
+                </motion.p>
+                <motion.p
+                  animate={{ x: isAnimating ? 0 : -200 }}
+                  style={{ x: -200, opacity: isAnimating ? 1 : 0 }}
+                >
+                  {playerData.data[selectedIndex].role}
+                </motion.p>
               </div>
             </div>
+            <div className="col-md-8 col-sm-12">
+              {playerData.label.map((chart, index) => (
+                <>
+                  <div
+                    key={index}
+                    className="col-md-4 col-sm-4"
+                    id={"player-chart-" + index}
+                  />
+                  {/* <div>{chart.name}</div> */}
+                </>
+              ))}
+            </div>
           </div>
-        ) : (
-          "loading"
-        )}
+        </div>
         <div id="row" className="col-md-12 section-title">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div
-                  key={`${d.name}-${i}`}
-                  className="col-md-1 col-sm-1 team"
-                  onClick={() => onChangePlayer(i)}
-                >
-                  <div className="thumbnail">
-                    {" "}
-                    <img src={d.img} alt={d.name} className="team-img" />
-                    <div className="caption">
-                      <div>{d.name}</div>
-                    </div>
-                  </div>
+          {playerData.data.map((d, i) => (
+            <div
+              key={`${d.name}-${i}`}
+              className="col-md-1 col-sm-1 team"
+              onClick={() => onChangePlayer(i)}
+            >
+              <div className="thumbnail">
+                {" "}
+                <img src={d.img} alt={d.name} className="team-img" />
+                <div className="caption">
+                  <div>{d.name}</div>
                 </div>
-              ))
-            : "loading"}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
